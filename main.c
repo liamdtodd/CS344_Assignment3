f07ffa4a3718ff6ee100229e6e9b840d4999da19
Liam Todd <toddl@os1.engr.oregonstate.edu>
2022-10-31 17:49:53 -0700

    'cd' implemented, mem errors, but works

--- main.c
+++ main.c
@@ -5,6 +5,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <unistd.h>
+
+void changeDIR(char*);
 
 int main() {
 	char* readstr = malloc(2048 * sizeof(char));
@@ -18,9 +25,39 @@ int main() {
 		printf(": ");
 		fflush(stdout);
 		bytes_read = getline(&readstr, &buffer_size, stdin);
+	
+		if (strncmp(readstr, "cd", 2) == 0)  
+			changeDIR(readstr);
 	}
 
 	free(readstr);
 	readstr = NULL;				//free dynamic mem
 	return 0;
+}
+
+//this function will change to the desired directory
+void changeDIR(char* tempstr) {
+	char* desiredDIR = malloc((strlen(tempstr) + 1) * sizeof(char));
+	strncpy(desiredDIR, tempstr, strlen(tempstr) - 1);	
+	desiredDIR += 3;			//set pointer to user's input, but after the 'cd', will store name of directory
+
+	int dirint = -5;
+
+	DIR* directory = opendir(".");
+	
+	char path[100];
+//	printf("pwd: %s\n", getcwd(path, sizeof(path)));
+
+	dirint = chdir(desiredDIR); 
+	if (dirint != 0) {
+		printf("error opening to directory: %s\n", desiredDIR);
+		fflush(stdout);
+	}
+
+//	printf("pwd: %s\n", getcwd(path, sizeof(path)));
+	closedir(directory);
+
+	desiredDIR -= 3;
+	free(desiredDIR);
+	desiredDIR = NULL;
 }
