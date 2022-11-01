fb77f227c9b5fc63b2c26d8e49458921b6fb77cf
liamdtodd <91576595+liamdtodd@users.noreply.github.com>
2022-10-31 21:28:14 -0700

    Create node.h

---
+++ node.h
@@ -0,0 +1,13 @@
+/******************
+ * Liam Todd
+ * The node struct for the struct Linked_List
+ * ***************/
+#ifndef NODE_H
+#define NODE_H
+
+struct node {
+	char* readstr;
+	struct node* next;
+};
+
+#endif
