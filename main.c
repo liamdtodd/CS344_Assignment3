adcfce5cd96e31c49896887d1dbd9a4c1693ec30
Liam Todd <toddl@os1.engr.oregonstate.edu>
2022-11-01 23:33:44 -0700

    linkedlist implemented, look into diff struct

--- main.c
+++ main.c
@@ -12,7 +12,7 @@
 #include <dirent.h>
 #include <unistd.h>
 
-void changeDIR(char*);
+void changeDIR(struct Linked_List*);
 void parseInput(struct Linked_List*, char*);
 
 int main() {
@@ -46,28 +46,13 @@ int main() {
 }
 
 //this function will change to the desired directory
-void changeDIR(char* tempstr) {
-	char* desiredDIR = malloc((strlen(tempstr) + 1) * sizeof(char));
-	strncpy(desiredDIR, tempstr, strlen(tempstr) - 1);	
-	desiredDIR += 3;			//set pointer to user's input, but after the 'cd', will store name of directory
-
-	int dirint = -5;
+void changeDIR(struct Linked_List* list) {
+	struct node* temp = list->head->next;
 
 	DIR* directory = opendir(".");
+	chdir(temp->readstr);
 	
-	char path[100];
-
-	dirint = chdir(desiredDIR); 
-	if (dirint != 0) {
-		printf("error opening to directory: %s\n", desiredDIR);
-		fflush(stdout);
-	}
-
 	closedir(directory);
-
-	desiredDIR -= 3;
-	free(desiredDIR);
-	desiredDIR = NULL;
 }
 
 //this function will parse through the user's input and store each string of chars b/w spaces into a node of a linkedlist
@@ -76,9 +61,12 @@ void parseInput(struct Linked_List* list, char* readstr) {
 	char* token = NULL;
 	char* tknptr = NULL;
 
-	token = strtok_r(readstr, " ", &tknptr);
-	while (token != NULL) {
+//	token = strtok_r(readstr, " ", &tknptr);
+	while ((token = strtok_r(readstr, " ", &tknptr)) != NULL) {
 		add_back(list, token);
-		token = strtok(readstr, " ", &tknptr);
+		token = strtok_r(readstr, " ", &tknptr);
 	}
+
+	if (strcmp(list->head->readstr, "cd") == 0)
+		changeDIR(list);
 }	
